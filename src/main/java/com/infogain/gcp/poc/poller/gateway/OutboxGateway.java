82d1248d3ddd0862df73a84719e47f920bcee0f6
initial-poc <gupta.hemanshu@gmail.com>
2021-05-02 19:06:57 +0530

    added scheduler

--- src/main/java/com/infogain/gcp/poc/poller/gateway/OutboxGateway.java
+++ src/main/java/com/infogain/gcp/poc/poller/gateway/OutboxGateway.java
@@ -5,6 +5,7 @@ import java.net.URI;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -14,6 +15,7 @@ import com.infogain.gcp.poc.poller.domainmodel.PNRModel;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 @Component
 @RequiredArgsConstructor
@@ -37,12 +39,22 @@ public class OutboxGateway {
 		return response;
 	}
 
+	public static ThreadPoolTaskExecutor taskExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(5);
+		executor.setMaxPoolSize(10);
+		executor.setQueueCapacity(100);
+		executor.setThreadNamePrefix("outbox-");
+		executor.initialize();
+		return executor;
+	}
+
 	public Mono<String> callService(PNRModel req) {
 		Mono<String> response = null;
 
 		response = webClient.post().uri("/api/pnrs").body(Mono.just(req), PNRModel.class).retrieve()
 				.onStatus((HttpStatus::isError), (it -> handleError(it.statusCode().getReasonPhrase())))
-				.bodyToMono(String.class);
+				.bodyToMono(String.class).publishOn(Schedulers.fromExecutor(taskExecutor()));
 
 		log.info("service called");
 		return response;
