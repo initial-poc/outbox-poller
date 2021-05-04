0d579ae88a7be049c1510fa85002235f99c5836d
initial-poc <gupta.hemanshu@gmail.com>
2021-05-04 15:41:58 +0530

    Added provision for Emulator

--- src/main/java/com/infogain/gcp/poc/App.java
+++ src/main/java/com/infogain/gcp/poc/App.java
@@ -15,4 +15,6 @@ public class App {
 		SpringApplication.run(App.class, args);
 	}
 
+
+
 }
--- src/main/java/com/infogain/gcp/poc/config/AppConfig.java
+++ src/main/java/com/infogain/gcp/poc/config/AppConfig.java
@@ -1,5 +1,8 @@
 package com.infogain.gcp.poc.config;
 
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpHeaders;
@@ -9,41 +12,34 @@ import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
-
-import io.netty.channel.ChannelOption;
-import io.netty.handler.timeout.ReadTimeoutHandler;
-import io.netty.handler.timeout.WriteTimeoutHandler;
 import reactor.netty.http.client.HttpClient;
 
 @Component
 public class AppConfig {
 
-	@Value(value = "${serviceUrl}")
-	private String seriveUrl="localhost:9000";
-	
-	@Bean
- public RestTemplate restTemplate() {
-	 return new RestTemplate();
- }
- 
- 
- 
- @Bean
-	public WebClient getWebClient()
-	{
-		HttpClient httpClient = HttpClient.create()
-		        .tcpConfiguration(client ->
-		                client.option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 100000)
-		                .doOnConnected(conn -> conn
-		                        .addHandlerLast(new ReadTimeoutHandler(10000))
-		                        .addHandlerLast(new WriteTimeoutHandler(10000))));
-		
-		ClientHttpConnector connector = new ReactorClientHttpConnector(httpClient.wiretap(true));	    
+    @Value(value = "${serviceUrl}")
+    private String seriveUrl = "localhost:9000";
 
-		return WebClient.builder()
-		        .baseUrl("http://"+seriveUrl)
-		        .clientConnector(connector)
-		        .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-		        .build();
-	}
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public WebClient getWebClient() {
+        HttpClient httpClient = HttpClient.create()
+                .tcpConfiguration(client ->
+                        client.option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 100000)
+                                .doOnConnected(conn -> conn
+                                        .addHandlerLast(new ReadTimeoutHandler(10000))
+                                        .addHandlerLast(new WriteTimeoutHandler(10000))));
+
+        ClientHttpConnector connector = new ReactorClientHttpConnector(httpClient.wiretap(true));
+
+        return WebClient.builder()
+                .baseUrl("http://" + seriveUrl)
+                .clientConnector(connector)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+    }
 }
